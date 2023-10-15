79d50a75a19fb736266c000e7dffe12c435df178
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2023-10-15 22:32:38 +0530

    task 3

---
+++ dom.js
@@ -0,0 +1,11 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header')
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+// headerTitle.innerText= 'Goodbye';
+//headerTitle.innerHTML = <h3> Hello</h3>
+//  headerTitle.style.border = 'solid 3px #000'
+header.style.border = 'solid 3px #000'
+
+var add = document.getElementById('main');
+add.style.color= 'green'
