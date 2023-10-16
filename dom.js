205373c90cf76a8c6b71cc6974ccf3be7b468e44
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2023-10-16 13:43:19 +0530

    task 4

--- dom.js
+++ dom.js
@@ -7,5 +7,17 @@ headerTitle.textContent = 'Hello';
 //  headerTitle.style.border = 'solid 3px #000'
 header.style.border = 'solid 3px #000'
 
-var add = document.getElementById('main');
-add.style.color= 'green'
+var add = document.getElementsByClassName('title');
+// add.style.backgroundColor = 'green'
+
+// get elements by class name // 
+var items =document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow'
+
+for(var i=0; i<items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4'
+}
